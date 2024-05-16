d753ee42afd97a3b053fd344839c397c7253be49
dakshu04 <dakshgrows@gmail.com>
2024-05-16 19:59:29 +0530

    Done with about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import  aboutImg  from "../assets/about.jpg";
+import { ABOUT_TEXT } from "../constants";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
@@ -9,11 +10,12 @@ const About = () => {
                 <div className="flex items-center justify-center">
                     <img className="rounded-2xl" src={aboutImg} alt="about" />
                 </div>
-
-                <div className="w-full lg:w-1/2">
-                    <div className="flex justify-center lg:justify-start"></div>
-                </div>
             </div>
+            <div className="w-full lg:w-1/2">
+                    <div className="flex justify-center lg:justify-start">
+                        <p className="my-2 max-w-xl py-6"> {ABOUT_TEXT}</p>
+                    </div>
+                </div>
         </div>
     </div>
   )
