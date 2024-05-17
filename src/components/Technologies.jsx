56646c160f6f9954dc6c37ead3bf5ad98d9e46e3
dakshu04 <dakshgrows@gmail.com>
2024-05-17 11:56:26 +0530

    Frame Motion is Happening

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,30 +4,66 @@ import { SiMongodb } from "react-icons/si";
 import { DiRedis } from "react-icons/di";
 import { FaNodeJs } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10], // Moves from 10 to -10
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse", // Reverses the direction each time
+    },
+  },
+});
 
 export default function Technologies() {
   return (
     <div className="border-b border-neutral-800 pb-24"><h1 className="my-20 text-center text-4xl">Technologies</h1>
     <div className="flex flex-wrap items-center justify-center gap-4">
-      <div className="rounded-2xl border-4 border-neutral-800">
+      
+      <motion.div 
+      variants={iconVariants(2.5)}
+      initial="initial" 
+      animate="animate"
+       className="rounded-2xl border-4 border-neutral-800 p-4">
+      
         <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
+      </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800">
+        <motion.div 
+        variants={iconVariants(3)}
+        initial="initial" 
+        animate="animate"
+         className="rounded-2xl border-4 border-neutral-800">
         <TbBrandNextjs className="text-7xl" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800">
+        <motion.div 
+        variants={iconVariants(2)}
+        initial="initial" 
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800">
         <SiMongodb className="text-7xl text-green-500" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800">
+        <motion.div 
+        variants={iconVariants(6)}
+        initial="initial" 
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800">
         <DiRedis className="text-7xl text-red-700" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800">
+        <motion.div 
+        variants={iconVariants(4)}
+        initial="initial" 
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800">
         <FaNodeJs className="text-7xl text-green-500" />
-        </div>
+        </motion.div>
 
         <div className="rounded-2xl border-4 border-neutral-800">
         <BiLogoPostgresql className="text-7xl text-sky-700" />
