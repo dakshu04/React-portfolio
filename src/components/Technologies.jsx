1e480effe67da543a794ae39412a97d8b57d0bbe
dakshu04 <dakshgrows@gmail.com>
2024-05-17 12:26:35 +0530

    Most Part is Done

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -21,14 +21,23 @@ const iconVariants = (duration) => ({
 
 export default function Technologies() {
   return (
-    <div className="border-b border-neutral-800 pb-24"><h1 className="my-20 text-center text-4xl">Technologies</h1>
-    <div className="flex flex-wrap items-center justify-center gap-4">
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1
+      whileInView={{opacity: 1, y: 0 }}
+      transition={{duration: 1.5 }}
+      initial={{ opacity: 0, y: -100 }}
+       className="my-20 text-center text-4xl">Technologies</motion.h1>
+    <motion.div
+     whileInView={{ opacity:1 , x: 0}}
+     initial={{ opacity: 0, x: -100 }}
+     transition = {{ duration : 1.5 }}
+    className="flex flex-wrap items-center justify-center gap-4">
       
       <motion.div 
       variants={iconVariants(2.5)}
       initial="initial" 
       animate="animate"
-       className="rounded-2xl border-4 border-neutral-800 p-4">
+       className="rounded-2xl border-4 border-neutral-800 p-0.5">
       
         <RiReactjsLine className="text-7xl text-cyan-400" />
       </motion.div>
@@ -69,7 +78,7 @@ export default function Technologies() {
         <BiLogoPostgresql className="text-7xl text-sky-700" />
         </div>
 
-      </div>
+      </motion.div>
     </div>
   )
 }
