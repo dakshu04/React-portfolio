dd1f2785bfdd52672fa4f5e8cf512b95963d264d
dakshu04 <dakshgrows@gmail.com>
2024-05-17 10:46:42 +0530

    Done with Frontend Move towards animation

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,7 +1,15 @@
 
+import {CONTACT } from "../constants";
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
+    <div className="border-b border-neutral-900 pb-20">
+      <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
+      <div className="text-center tracking-tighter">
+        <p className="m-4">{CONTACT.address}</p>
+        <p className="m-4">{CONTACT.phoneNo}</p>
+        <a href="#" className="border-b">{CONTACT.email}</a>
+      </div>
+      </div>
   )
 }
