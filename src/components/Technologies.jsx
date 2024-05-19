af81d80019ea7ead5ce4b83a0a0c628aecccffc2
dakshu04 <dakshgrows@gmail.com>
2024-05-20 01:19:01 +0530

    Done w/ Technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,9 @@
 import { RiReactjsLine } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
+import { DiHtml5 } from "react-icons/di";
 import { SiMongodb } from "react-icons/si";
-import { DiRedis } from "react-icons/di";
+import { DiJava } from "react-icons/di";
 import { FaNodeJs } from "react-icons/fa";
-import { BiLogoPostgresql } from "react-icons/bi";
+import { DiCss3Full } from "react-icons/di";
 import { motion } from "framer-motion";
 
 const iconVariants = (duration) => ({
@@ -26,7 +26,8 @@ export default function Technologies() {
       whileInView={{opacity: 1, y: 0 }}
       transition={{duration: 1.5 }}
       initial={{ opacity: 0, y: -100 }}
-       className="my-20 text-center text-4xl">Technologies</motion.h1>
+      className="my-20 text-center text-4xl">Technologies</motion.h1>
+
     <motion.div
      whileInView={{ opacity:1 , x: 0}}
      initial={{ opacity: 0, x: -100 }}
@@ -34,20 +35,36 @@ export default function Technologies() {
     className="flex flex-wrap items-center justify-center gap-4">
       
       <motion.div 
-      variants={iconVariants(2.5)}
-      initial="initial" 
-      animate="animate"
-       className="rounded-2xl border-4 border-neutral-800 p-0.5">
-      
-        <RiReactjsLine className="text-7xl text-cyan-400" />
-      </motion.div>
+        variants={iconVariants(2)}
+        initial="initial" 
+        animate="animate"
+         className="rounded-2xl border-4 text-orange-600 border-neutral-800">
+        <DiHtml5 className="text-7xl" />
+        </motion.div>
 
         <motion.div 
         variants={iconVariants(3)}
         initial="initial" 
         animate="animate"
-         className="rounded-2xl border-4 border-neutral-800">
-        <TbBrandNextjs className="text-7xl" />
+        className="rounded-2xl border-4 border-neutral-800">
+        <DiCss3Full className="text-7xl text-sky-700" />
+        </motion.div>
+      
+      <motion.div 
+      variants={iconVariants(2)}
+      initial="initial" 
+      animate="animate"
+       className="rounded-2xl border-4 border-neutral-800 p-0.5">
+      
+      <RiReactjsLine className="text-7xl text-cyan-400" />
+      </motion.div>
+
+      <motion.div 
+        variants={iconVariants(3.5)}
+        initial="initial" 
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800">
+        <FaNodeJs className="text-7xl text-green-500" />
         </motion.div>
 
         <motion.div 
@@ -57,26 +74,16 @@ export default function Technologies() {
         className="rounded-2xl border-4 border-neutral-800">
         <SiMongodb className="text-7xl text-green-500" />
         </motion.div>
+        
 
         <motion.div 
-        variants={iconVariants(6)}
+        variants={iconVariants(3)}
         initial="initial" 
         animate="animate"
         className="rounded-2xl border-4 border-neutral-800">
-        <DiRedis className="text-7xl text-red-700" />
+        <DiJava className="text-7xl text-red-700" />
         </motion.div>
 
-        <motion.div 
-        variants={iconVariants(4)}
-        initial="initial" 
-        animate="animate"
-        className="rounded-2xl border-4 border-neutral-800">
-        <FaNodeJs className="text-7xl text-green-500" />
-        </motion.div>
-
-        <div className="rounded-2xl border-4 border-neutral-800">
-        <BiLogoPostgresql className="text-7xl text-sky-700" />
-        </div>
 
       </motion.div>
     </div>
