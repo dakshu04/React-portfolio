23054ddae53e37b76ab2b0a222aa5f038d11604b
dakshu04 <dakshgrows@gmail.com>
2024-05-20 00:51:02 +0530

    Customize done w/ Navbar and Hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/DakshPurohitLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
-
+import { motion } from "framer-motion"
 export const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className= "mx-2 w-10" src={logo} alt= "logo" />
+            <motion.img
+            initial={{ opacity: 0 }}  // Initial opacity set to 0 (fully transparent)
+            animate={{ opacity: 1 }}  // Animate opacity to 1 (fully opaque)
+            transition={{ duration: 0.5, delay: 0.5 }}  // Transition duration and delay 
+            style={{ width: "50px", height: "50px", borderRadius: '20px'}}
+            className= "mx-2 w-10" src={logo} alt= "logo" />
         </div>
 
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
