872e47d500cbc57a675f14ed65cf18d80e660902
dakshu04 <dakshgrows@gmail.com>
2024-05-20 00:30:29 +0530

    Customising

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
   import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/DakshProfile.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
@@ -20,7 +20,7 @@ const Hero = () => {
                     <motion.h1 variants={container(0)}
                     initial="hidden"
                     animate="visible"
-                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Kevin Rush</motion.h1>
+                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Daksh Purohit</motion.h1>
 
                     <motion.span variants={container(0.5)}
                     initial="hidden"
@@ -33,11 +33,13 @@ const Hero = () => {
                     </motion.p>
                 </div>
             </div>
-            <div className="w-full lg:w-1/2 lg:p-8">
-                <div className="flex justify-center">
+            <div className="w-full lg:w-1/2  lg:p-8">
+                <div className="flex justify-center border-radius:25px">
                     <motion.img initial={{x:100, opacity:0}}
                     animate={{x:0, opacity:1}}
-                    transition={{duration: 1, delay:1.2}} src={profilePic} alt="Kevin Rush"/>
+                    transition={{duration: 1, delay:0.5}} src={profilePic} alt="Daksh Purohit"
+                    style={{ borderRadius: '20px' }}                  
+                    />
                 </div>
             </div>
         </div>
